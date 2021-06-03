1b637a419d816a2dae993446405d249229258850
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-03 15:37:15 +0900

    ユーザー edit,updateの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'tweets#index'
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   resources :tweets
 end
