6ecb696b87eb824522a32ac59ff1433e6ff5ef41
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-03 17:21:37 +0900

    マイページ実装(show)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user, only: :show
-  
+
   def show
-    @user = User.find(params[:id])
+    user = User.find(params[:id])
+    @nickname = user.nickname 
+    @tweets = user.tweets
   end
 
 end
