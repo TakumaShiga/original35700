02d01ea7c80ff3d96f66e0a82cfd39d5e8368bb7
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-03 16:59:09 +0900

    ユーザーテストコード復活させた

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname               {Faker::Name.name}
+    email                  {Faker::Internet.free_email}
+    full_name              {"さくらももこ"}
+    password               {'1a' + Faker::Internet.password(min_length: 6)}
+    password_confirmation  {password}
+    gender                 {"male"}
+    birth_day              {19860715}
   end
 end
