0607994f70ac7a71cdb0050c27ee6b0f2fe05dd6
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-04 20:39:55 +0900

    likesルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'tweets#index'
   resources :users, only: [:show, :edit, :update]
   resources :tweets do
+    resource :likes, only: [:create, :destroy]
     resources :comments, only: :create
     collection do
       get 'search'
