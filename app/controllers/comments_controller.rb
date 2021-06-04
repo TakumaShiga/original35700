11c9caa191798ee9881382827639cad871517158
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-04 11:59:44 +0900

    commentコントローラー設定

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+
+  def create
+   comment = Comment.create(comment_params)
+   redirect_to tweet_path(comment.tweet_id) 
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+end
