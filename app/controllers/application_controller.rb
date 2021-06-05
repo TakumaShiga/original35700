d83f0f8b29f11f20f790e9578d9b5ce5c0d2d31a
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-05 12:21:41 +0900

    ストロングパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :full_name, :gender, :birth_day, :introduce, :profile_image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :full_name, :gender, :birth_day, :introduce, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:introduce, :image])
   end
 end
