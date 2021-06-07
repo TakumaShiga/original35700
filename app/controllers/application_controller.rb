f301bb817da11a74b6e506f1c8cb43919de5ae93
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-07 11:43:29 +0900

    ユーザー編集機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :full_name, :gender, :birth_day, :introduce, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:introduce, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :email, :introduce, :image])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user, only: :show
+  before_action :authenticate_user!, only: :show
+  
 
   def show
-    user = User.find(params[:id])
-    @nickname = user.nickname 
-    @tweets = user.tweets
+    @user = User.find(params[:id])
+    @nickname = @user.nickname 
+    @tweets = @user.tweets
   end
-
+  
 end
