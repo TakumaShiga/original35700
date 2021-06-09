1fd5bb2c3d9af2c696c53ecca0fb1cebc82695c2
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-09 14:52:10 +0900

    文字数制限修正

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ApplicationRecord
-  validates :message, presence: true, length: { maximum: 600 }
+  validates :message, presence: true, length: { maximum: 500 }
 
   belongs_to :question
   belongs_to :user
