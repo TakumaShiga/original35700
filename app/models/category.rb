b0f1423d03ded86842b8d1ce74e63cf9f75605bb
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-11 14:56:01 +0900

    ActiveHashの導入

---
+++ app/models/category.rb
@@ -0,0 +1,15 @@
+class Category < ActiveHash::Base
+  self.data =[
+    {id: 1, name: '---'},
+    {id: 2, name: 'ランニング練習会'},
+    {id: 3, name: '1km'},
+    {id: 4, name: '3km'},
+    {id: 5, name: '5km'},
+    {id: 7, name: '10km'},
+    {id: 8, name: 'ハーフ(21km)'},
+    {id: 9, name: 'フルマラソン(42km)'}
+  ]
+
+  include ActiveHash::Associations
+  has_many :events
+end
