dac222aca51c0b44e32b3babb9df2ca37d41cee5
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-12 21:58:45 +0900

    uniqueインデックスを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_11_051141) do
+ActiveRecord::Schema.define(version: 2021_06_12_125022) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -117,6 +117,7 @@ ActiveRecord::Schema.define(version: 2021_06_11_051141) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["event_id"], name: "index_registrations_on_event_id"
+    t.index ["user_id", "event_id"], name: "index_registrations_on_user_id_and_event_id", unique: true
     t.index ["user_id"], name: "index_registrations_on_user_id"
   end
 
