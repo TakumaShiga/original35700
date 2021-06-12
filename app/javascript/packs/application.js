facca112ac2a3c66df4f08f4186a04f37f387539
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-12 21:54:19 +0900

    turbolinks削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,13 +4,13 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-// require("turbolinks").start()
+
 require("@rails/activestorage").start()
 require("channels");
 require('jquery')
 require('../preview')
 require('../entry_fee')
-require('../card.js')
+require('../card')
 $(function(){
   setTimeout("$('.notifications').fadeOut('slow')", 2000);
 });
