32ef664b6b1c2df87243ada31350dc8ac1cf25aa
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-13 08:46:00 +0900

    registration_addressバリデーション修正

--- app/models/registration_address.rb
+++ app/models/registration_address.rb
@@ -1,20 +1,24 @@
 class RegistrationAddress
   include ActiveModel::Model
   attr_accessor :postal_code, :prefecture_id, :city, :place_number, :building_name, :phone_number, :user_id, :event_id, :token
-
+  
   with_options presence: true do
     validates :user_id
     validates :event_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフンを含む半角英数字で入力してください"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフンを含む半角数字で入力してください"}
     validates :city
     validates :place_number
-    validates :phone_number
     validates :token
+    with_options numericality: true do
+      validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "は10桁もしくは11桁で入力してください"}
+    end
   end
 
-    validates :event_id, uniqueness: { scope: :user_id }
-    validates :prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
-    validates :phone_number, numericality: {with: /\A\d{10,11}\z/, message: "は11桁以内の半角数字で入力してください"}
+  validates :prefecture_id, numericality: {other_than: 1, message: "を選択してください"}
+
+ 
+  
+   
 
     def save
       registration = Registration.create(user_id: user_id, event_id: event_id)
