60f4423757f5a2576adca4ea7cce74975ccfb294
TakumaShiga <tak07sweetvo@gmail.com>
2021-06-22 22:50:43 +0900

    バリデーションの修正

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index]
   before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update, :destroy]
 
